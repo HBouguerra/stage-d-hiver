9a55092ec44196af80e6a9b10d04d9010237983b
HBouguerra <156515889+HBouguerra@users.noreply.github.com>
2024-01-25 10:42:15 +0100

    mise en dev

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="app.css">          
+</head>
+<body>
+<article>
+    <h1>My first post</h1>
+    <p>
+    Un paragraphe est une section de texte en prose vouée au développement d'un point particulier souvent au moyen de plusieurs phrases, dans la continuité du précédent et du suivant. Sur le plan typographique, le paragraphe est compris entre deux alinéas, qui s'analysent aussi comme une « ponctuation blanche
+</p>
+
+</article>
+<a href="/">Go Back</a>
+</body>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+Route::get('post', function () {
+    return view('post');
+});
+
+Route::get('posta', function () {
+    return view('posta');
+});
+
+Route::get('postb', function () {
+    return view('postb');
 });
